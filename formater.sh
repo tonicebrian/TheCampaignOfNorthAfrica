f2695a4156577e15bddd77309a1ef33f95235185
Toni Cebrián <toni@tonicebrian.com>
2026-01-01 21:08:37 +0100

    Added all sections

--- formater.sh
+++ formater.sh
@@ -7,7 +7,7 @@ perl -pi -e 's/\[(\d+)\.0\]/[#\1_0]\n==/g' chapter.adoc
 perl -pi -e 's/\[(\d+)\.(\d)\]/[#\1_\2]\n===/g' chapter.adoc
 #perl -pi -e 's/\[\(\d\).\(\d\)\]/[#\1_\2]\r===/' chapter.adoc
 # Add anchors for paragrah sections [3.23]
-perl -pi -e 's/\[(\d+)\.(\d\d)\]/[#\1_\2]*[\1.\2]*/g' chapter.adoc
+perl -pi -e 's/^.(\d+)\.(\d\d)\]/[#\1_\2]*[\1.\2]*/g' chapter.adoc
 #perl -pi -e 's/\[\(\d\).\(\d\d\)\]/[#\1_\2]*[\1.\2]*/' chapter.adoc
 # Remove wong quotes
 perl -pi -e 's/’’/"/g' chapter.adoc
